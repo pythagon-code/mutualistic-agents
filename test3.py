b59ef64849bddb8584b2ce38885d14ef268bd592
Abhay Pokhriyal <abhaypokh89@gmail.com>
2026-03-09 15:37:34 -0500

    Update

--- test3.py
+++ test3.py
@@ -2,12 +2,14 @@ from fnn import FNN
 import torch
 from torch import Tensor, nn, optim
 from torch.nn.functional import mse_loss
-from torch.nn import CosineSimilarity
 from torch.nn.utils import clip_grad_norm_
 from tqdm import trange
-from typing import TypeVar
+from typing import Callable, TypeVar
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+ACTOR_POLYAK_FACTOR = 0.0001
+CRITIC_POLYAK_FACTOR = 0.01
 
 T = TypeVar("T", float, Tensor)
 
@@ -21,12 +23,17 @@ def polyak_update(target: nn.Module, online: nn.Module, polyak_factor: float) ->
         target_param.data.copy_(get_ema(target_param.data, online_param.data, polyak_factor))
 
 
+def cos_sim_of_cube(vec_a: Tensor, vec_b: Tensor) -> Tensor:
+    num = (vec_a ** 3 * vec_b ** 3).sum(dim = -1)
+    denom = vec_a.pow(3).norm(dim = -1) * vec_b.pow(3).norm(dim = -1) + 1e-8
+    return num / denom
+
+
 class Actor(nn.Module):
     def __init__(self, vec_dim: int, embed_dim: int) -> None:
         super().__init__()
         self._vec_dim = vec_dim
         self._embed_dim = embed_dim
-        self._cos_sim = CosineSimilarity(dim = -1)
         self._enc_a = FNN(
             input_size = vec_dim,
             hidden_size = 16,
@@ -92,69 +99,78 @@ class Actor(nn.Module):
             embed_b_head = self._enc_b_target(vec_b)
         head_in = torch.cat([embed_a_head, embed_b_head], dim = -1)
         pred_head = self._head(head_in).squeeze(-1)
-        critic_loss_head = critic.forward(
+        actor_loss_head = critic.forward(
             vec_a = vec_a,
             vec_b = vec_b,
             embed_a = embed_a_head,
             embed_b = embed_b_head,
             pred = pred_head,
         )
-        head_loss = critic_loss_head.mean()
+        head_loss = actor_loss_head.mean()
         self._head_opt.zero_grad(set_to_none = True)
         head_loss.backward()
         clip_grad_norm_(self._head.parameters(), max_norm = 1.0)
         self._head_opt.step()
-        polyak_update(self._head_target, self._head, 0.01)
+        polyak_update(self._head_target, self._head, ACTOR_POLYAK_FACTOR)
 
         with torch.no_grad():
             embed_b_for_a = self._enc_b_target(vec_b)
         embed_a = self._enc_a(vec_a)
         head_in_a = torch.cat([embed_a, embed_b_for_a], dim = -1)
         pred_a = self._head_target(head_in_a).squeeze(-1)
-        critic_loss_a = critic.forward(
+        actor_loss_a = critic.forward(
             vec_a = vec_a,
             vec_b = vec_b,
             embed_a = embed_a,
             embed_b = embed_b_for_a,
             pred = pred_a,
         )
-        enc_a_loss = critic_loss_a.mean()
+        enc_a_loss = actor_loss_a.mean()
         self._enc_a_opt.zero_grad(set_to_none = True)
         enc_a_loss.backward()
         clip_grad_norm_(self._enc_a.parameters(), max_norm = 1.0)
         self._enc_a_opt.step()
-        polyak_update(self._enc_a_target, self._enc_a, 0.01)
+        polyak_update(self._enc_a_target, self._enc_a, ACTOR_POLYAK_FACTOR)
 
         with torch.no_grad():
             embed_a_for_b = self._enc_a_target(vec_a)
         embed_b = self._enc_b(vec_b)
         head_in_b = torch.cat([embed_a_for_b, embed_b], dim = -1)
         pred_b = self._head_target(head_in_b).squeeze(-1)
-        critic_loss_b = critic.forward(
+        actor_loss_b = critic.forward(
             vec_a = vec_a,
             vec_b = vec_b,
             embed_a = embed_a_for_b,
             embed_b = embed_b,
             pred = pred_b,
         )
-        enc_b_loss = critic_loss_b.mean()
+        enc_b_loss = actor_loss_b.mean()
         self._enc_b_opt.zero_grad(set_to_none = True)
         enc_b_loss.backward()
         clip_grad_norm_(self._enc_b.parameters(), max_norm = 1.0)
         self._enc_b_opt.step()
-        polyak_update(self._enc_b_target, self._enc_b, 0.01)
+        polyak_update(self._enc_b_target, self._enc_b, ACTOR_POLYAK_FACTOR)
 
 
-    def eval_mse(self, batch_size: int = 1024) -> Tensor:
+    def eval_mse(self, critic: "Critic", target_fn: Callable, batch_size: int = 4096) -> tuple[Tensor, Tensor, Tensor]:
         with torch.no_grad():
             vec_a = torch.rand(batch_size, self._vec_dim, device = device)
             vec_b = torch.rand(batch_size, self._vec_dim, device = device)
-            target_cos = self._cos_sim(vec_a ** 3, vec_b ** 3)
-            embed_a = self._enc_a_target(vec_a)
-            embed_b = self._enc_b_target(vec_b)
-            head_in = torch.cat([embed_a, embed_b], dim = -1)
-            pred = self._head_target(head_in).squeeze(-1)
-            return mse_loss(pred, target_cos)
+            target = target_fn(vec_a, vec_b)
+            vec_a, vec_b, embed_a, embed_b, pred, target = self.forward(vec_a, vec_b, target)
+            actor_mse = mse_loss(pred, target)
+
+            critic_pred = critic.forward(
+                vec_a = vec_a,
+                vec_b = vec_b,
+                embed_a = embed_a,
+                embed_b = embed_b,
+                pred = pred,
+            )
+            actor_loss = critic_pred.mean()
+            target_loss = (pred - target) ** 2
+            critic_mse = mse_loss(critic_pred, target_loss)
+            return actor_mse, actor_loss, critic_mse
 
 
 class Critic(nn.Module):
@@ -251,7 +267,7 @@ class Critic(nn.Module):
         head_loss.backward()
         clip_grad_norm_(self._head.parameters(), max_norm = 1.0)
         self._head_opt.step()
-        polyak_update(self._head_target, self._head, 0.01)
+        polyak_update(self._head_target, self._head, CRITIC_POLYAK_FACTOR)
 
         with torch.no_grad():
             state_action_b = torch.cat([vec_b, embed_b], dim = -1)
@@ -265,7 +281,7 @@ class Critic(nn.Module):
         enc_a_loss.backward()
         clip_grad_norm_(self._enc_a.parameters(), max_norm = 1.0)
         self._enc_a_opt.step()
-        polyak_update(self._enc_a_target, self._enc_a, 0.01)
+        polyak_update(self._enc_a_target, self._enc_a, CRITIC_POLYAK_FACTOR)
 
         with torch.no_grad():
             state_action_a = torch.cat([vec_a, embed_a], dim = -1)
@@ -279,26 +295,24 @@ class Critic(nn.Module):
         enc_b_loss.backward()
         clip_grad_norm_(self._enc_b.parameters(), max_norm = 1.0)
         self._enc_b_opt.step()
-        polyak_update(self._enc_b_target, self._enc_b, 0.01)
+        polyak_update(self._enc_b_target, self._enc_b, CRITIC_POLYAK_FACTOR)
 
 
-    def eval_mse(self, actor: Actor, batch_size: int = 4096) -> Tensor:
+    def eval_mse(self, actor: Actor, batch_size: int, target_fn) -> Tensor:
         with torch.no_grad():
             vec_a = torch.rand(batch_size, self._vec_dim, device = device)
             vec_b = torch.rand(batch_size, self._vec_dim, device = device)
-            embed_a = actor._enc_a_target(vec_a)
-            embed_b = actor._enc_b_target(vec_b)
-            actor_head_in = torch.cat([embed_a, embed_b], dim = -1)
-            pred = actor._head_target(actor_head_in).squeeze(-1)
-            target = actor._cos_sim(vec_a ** 3, vec_b ** 3)
+            target = target_fn(vec_a, vec_b)
+            vec_a, vec_b, embed_a, embed_b, pred, target = actor.forward(vec_a, vec_b, target)
             target_loss = (pred - target) ** 2
-            state_action_a = torch.cat([vec_a, embed_a], dim = -1)
-            state_action_b = torch.cat([vec_b, embed_b], dim = -1)
-            critic_embed_a = self._enc_a_target(state_action_a)
-            critic_embed_b = self._enc_b_target(state_action_b)
-            head_in = torch.cat([critic_embed_a, critic_embed_b, pred.unsqueeze(-1)], dim = -1)
-            pred_loss = self._head_target(head_in).squeeze(-1)
-            return mse_loss(pred_loss, target_loss)
+            critic_pred = self.forward(
+                vec_a = vec_a,
+                vec_b = vec_b,
+                embed_a = embed_a,
+                embed_b = embed_b,
+                pred = pred,
+            )
+            return mse_loss(critic_pred, target_loss)
 
 
 def test_mutual_cosine_similarity() -> None:
@@ -314,18 +328,27 @@ def test_mutual_cosine_similarity() -> None:
     for i in trange(20000, desc = "mutual cosine"):
         vec_a = torch.rand(128, 4, device = device)
         vec_b = torch.rand(128, 4, device = device)
-        with torch.no_grad():
-            target = actor._cos_sim(vec_a ** 3, vec_b ** 3)
+        target = cos_sim_of_cube(vec_a, vec_b)
         critic_params = actor.forward(vec_a, vec_b, target)
         actor.train(critic, vec_a, vec_b, target)
         critic.train(*critic_params)
-        if i % 250 == 0:
-            actor_mse = actor.eval_mse()
-            critic_mse = critic.eval_mse(actor = actor)
-            print(f"step: {i}, actor_mse: {actor_mse.item()}, critic_mse: {critic_mse.item()}")
+        if i % 100 == 0:
+            actor_mse, actor_loss, critic_mse = actor.eval_mse(
+                critic = critic,
+                target_fn = cos_sim_of_cube,
+                batch_size = 1024,
+            )
+            print(
+                f"step: {i}, actor_mse: {actor_mse.item():.10f}, "
+                f"critic_loss: {critic_loss.item():.10f}, critic_mse: {critic_mse.item():.10f}"
+            )
 
-    final_mse = actor.eval_mse(batch_size = 4096)
-    assert final_mse < 5e-3
+    final_actor_mse, final_critic_loss, final_critic_mse = actor.eval_mse(
+        critic = critic,
+        target_fn = cos_sim_of_cube,
+        batch_size = 4096,
+    )
+    assert final_actor_mse < 5e-3
 
 
 if __name__ == "__main__":
